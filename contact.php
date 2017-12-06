e6221a5f9426068fcd24f9bc142f5c7a8da1f14c
Sergii <sergiigrytsaienko@gmail.com>
2017-12-06 00:36:23 +0000

    contact form fixed, not it works properly

--- contact.php
+++ contact.php
@@ -1,56 +1,80 @@
-<?php include 'includes/header.php';
+<?php include 'includes/header.php'; ?>
 
-function died($error)
-{
-    echo "We are very sorry, but there were error(s) found with the form you submitted.";
-    echo "These errors appear below.<br><br>";
-    echo $error . "<br><br>";
-    echo "Please go back and fix these errors.<br><br>";
-    die();
-}
-
-function clean_input($data)
-{
-    global $connection;
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    $data = mysqli_real_escape_string($connection, $data);
-    return $data;
-}
-
-
+<?php
+    $name = '';
+    $subject = '';
+    $email = '';
+    $message = '';
+    
+    $nameError = '';
+    $subjectError = '';
+    $emailError = '';
+    $messageError = '';
+    $success = '';
+    
 if (isset($_POST['send'])) {
+    
+        function clean_input($data)
+    {
+        global $connection;
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        $data = mysqli_real_escape_string($connection, $data);
+        return $data;
+    }
+    
+        if (empty($_POST["name"])){
+        $nameError = "Name is required";
+    }
+    else
+     {
+        $name = clean_input($_POST["name"]); 
+        if (!preg_match("/^[a-zA-Z ]*$/",$name))
+        {
+        $nameError = "Only letters and white space allowed";
+        }
+    } 
+    
 
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+    if (empty($_POST["email"]))
+    {
+        $emailError = "Email is required";
+    }
+    else
+     {
+        $email = clean_input($_POST["email"]);
+    } 
+    
+    if (preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email))
+        {
+            $success = "Message sent successfully.";
+        } 
+        else 
+        {
+            $success = "Invalid Email";
+        }
 
-    $string_exp = "/^[A-Za-z0-9 .'-]+$/";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-    $error = '';
-
-    if (!preg_match($string_exp, $name)) {
-        $error .= "The First Name you entered does not appear to be valid.<br>";
-    }
-    if (!preg_match($string_exp, $subject)) {
-        $error .= "The Subject you entered do not appear to be valid.<br>";
-    }
-    if (!preg_match($email_exp, $email)) {
-        $error .= "The Email you entered do not appear to be valid.<br>";
-    }
-    if (!preg_match($string_exp, $message)) {
-        $error .= "The Comments you entered do not appear to be valid.<br>";
-    }
-    if (strlen($error) > 0) {
-        died($error);
+    if (empty($_POST["subject"]))
+    {
+        $subjectError = "Purpose is required";
+    } 
+    else
+    {
+        $subject = clean_input($_POST["purpose"]);
     }
 
-    $name = clean_input($name);
-    $subject = clean_input($subject);
-    $email = clean_input($email);
-    $message = clean_input($message);
+    if (empty($_POST["message"]))
+    {
+        $messageError = "Message is required";
+    }
+    else
+     {
+        $message = clean_input($_POST["message"]);
+    }
+    
+    
+
 
 
 //get email from DB
@@ -60,47 +84,59 @@ if (isset($_POST['send'])) {
         'X-Mailer: PHP/' . phpversion();
 
     $result = mail($admin_email, $subject, $message, $headers);
-    var_dump($result);
+//    header('Location: contact.php');
     if (!$result) {
-        echo "<script>alert('Wrong')</script>";
-    } else {
-        echo "<h1>SENT</h1>";
+        echo "
+        <script>
+        
+        
+        alert('Something is wrong');
+        
+        
+        </script>";
     }
 }
 
 ?>
-
     <section id="contact" class="content-section text-center">
         <div class="contact-section">
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
-                    <form class="form-horizontal" method="post">
+                    <form class="form-horizontal" method="post" action="contact.php">
                         <h2>Contact Me</h2>
                         <div class="form-group">
                             <label for="exampleInputName2"><b>Name</b></label>
                             <input type="text" class="form-control" id="exampleInputName2" placeholder="Name"
                                    name="name">
+                            <span class="error"><?php echo $nameError;?></span>
 
                         </div>
                         <div class="form-group">
                             <label for="exampleInputName2"><b>Subject</b></label>
                             <input type="text" class="form-control" id="exampleInputName2" placeholder="Subject"
                                    name="subject">
+                            <span class="error"><?php echo $subjectError;?></span>
                         </div>
                         <div class="form-group">
                             <label for="exampleInputEmail2"><b>Email</b></label>
                             <input type="email" class="form-control" id="exampleInputEmail2"
                                    placeholder="Email" name="email">
+                            <span class="error"><?php echo $emailError;?></span>
                         </div>
                         <div class="form-group ">
                             <label for="exampleInputText"><b>Your Message</b></label>
                             <textarea class="form-control" placeholder="Description" name="message"></textarea>
+                            <span class="error"><?php echo $messageError;?></span>
                         </div>
                         <button type="submit" class="btn btn-default sendButton" name="send"><b>Send Message</b>
                         </button>
+                            <span class="success"><?php echo "<br>" . $success;?></span>
                     </form>
                 </div>
             </div>
         </div>
     </section>
 <?php include 'includes/footer.php'; ?>
+
+
+
--- db/global_config.php
+++ db/global_config.php
@@ -8,10 +8,7 @@ define("DB_PASS",'');
 define("DB_NAME",'photography');
 
 //hosting
-//define("DB_HOST",'localhost');
-//define("DB_USER",'id2478934_sgryt');
-//define("DB_PASS",'sgryt2010');
-//define("DB_NAME",'id2478934_photography');
+
 
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
